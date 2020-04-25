f9b202607a71a22ad056fab91e164f40311b001e
Christopher Jalen Adatan <jadatan1019@gmail.com>
2020-04-25 04:46:06 -0700

    Refactor renderer.js
    Squashed commit of the following:
    
    commit dd167e7affb5e9f3113f99e3df8733fc3612b0b3
    Author: Christopher Jalen Adatan <jadatan1019@gmail.com>
    Date:   Sat Apr 25 04:13:30 2020 -0700
    
        Refactor addNoteHandler
    
    commit b70a0af88039535c34af081711698380a3f380ec
    Author: Christopher Jalen Adatan <jadatan1019@gmail.com>
    Date:   Sat Apr 25 03:40:33 2020 -0700
    
        Define NewNote class
    
    commit df061e1c96a9f4104173069b09b469cba1d28141
    Author: Christopher Jalen Adatan <jadatan1019@gmail.com>
    Date:   Sat Apr 25 03:11:35 2020 -0700
    
        Seperate handlers and listeners

--- renderer.js
+++ renderer.js
@@ -1,17 +1,27 @@
 let ipcRenderer = require('electron');
 
+// Custom element defintions
+class NewNote extends HTMLInputElement {
+    constructor() {
+        super();
+
+        // Define the new note attributes
+        this.id = "note-text";
+        this.type = "text";
+    }
+}
+
+// Handlers
+
+// Add note handler 
+var addNoteHandler = function() {
+    // define attributes for input tag
+    var inputField = document.createElement("input", { is: "NewNote" });
+
+    // add the newly created element before add-note button
+    addNote.parentElement.insertBefore(inputField, addNote);
+}
+
+// Event listeners
 var addNote = document.getElementById("add-note");
-
-addNote.addEventListener("click", () => {
-    console.log("add-note button clicked...");
-
-    console.log("Creating input field...")
-    // Define attributes for input tag
-    var inputField = document.createElement("input");
-        inputField.id = "note-text";
-        inputField.type = "text";
-
-    // Append inputField to add-note-container
-    var addNoteContainer = document.getElementById("add-note-container");
-    addNoteContainer.appendChild(inputField);
-});
+addNote.addEventListener("click", addNoteHandler, false);
