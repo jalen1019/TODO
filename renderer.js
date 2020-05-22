52ae3bddf04dac13606fdb7b6281a4a70670878d
Christopher Jalen Adatan <jadatan1019@gmail.com>
2020-05-21 21:30:00 -0700

    Require bundled version of bootstrap
    The bundled version of bootstrap includes popper, which is used by some
    components of bootstrap.

--- renderer.js
+++ renderer.js
@@ -1,4 +1,4 @@
 'use strict'
 
 window.$ = window.jQuery = require('./node_modules/jquery/dist/jquery.min.js');
-window.Bootstrap = require("./node_modules/bootstrap/dist/js/bootstrap.min.js");
+window.Bootstrap = require("./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js");
